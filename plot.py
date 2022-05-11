45a4f64b395dd10bc6aa86652125b475d4751eda
Kirill Varnatsov <kirill.varnatsov@gmail.com>
2022-05-11 17:40:08 +0300

    stuff

--- plot.py
+++ plot.py
@@ -4,11 +4,13 @@ import json
 import pandas as pd
 import numpy as np
 import csv
-
+import warnings
+from statsmodels.tools.sm_exceptions import ConvergenceWarning
+warnings.simplefilter('ignore', ConvergenceWarning)
+import scipy.stats
 # sns.color_palette("flare", as_cmap=True)
 # sns.set_theme(style="darkgrid")
 # sns.set_context("paper")
-plt.style.use(['science', 'grayscale', 'grid'])
 
 
 # topmost = [
@@ -66,7 +68,7 @@ font = {'family': 'monospace',
         'size': 20}
 
 
-PLOT_MARGIN = 0.1
+PLOT_MARGIN = 0.25
 HEIGHT = 1920
 WIDTH = 1200
 
@@ -113,35 +115,39 @@ def scatter_reg(filename, csv_filenames, plottable):
 
     # plt.axis([-15, 30, -15, 30])
     # plt.figure(figsize=(12, 8))
-    figure, axes = plt.subplots(2, 3, figsize=(18, 10))
+    figure, axes = plt.subplots(2, 3, figsize=(19, 11))
     for index_1, _ in enumerate(plottable):
         s_in = 0
         for p_df in plottable_df[index_1]:
             p_df = p_df.loc[:, ~p_df.columns.duplicated()]
+
             y_max, y_min = np.amax(p_df.iloc[:, 0]), np.amin(p_df.iloc[:, 0])
             x_max, x_min = np.amax(p_df.values), np.amin(p_df.values)
-            labels = []
+
             for _, d in enumerate(p_df):
                 if p_df.iloc[:, 0].name != d:
-                    truncate = True
                     if len(set(p_df[d])) > 1:
-                        truncate = False
-                    #print(set(p_df[d]))
-                    s = sns.regplot(ax=axes[index_1, s_in], y=p_df.iloc[:, 0].name, x=d,
-                                    data=p_df, ci=None, truncate=truncate, line_kws={'linewidth': 0.6}, scatter_kws={'alpha': 0.5})
-                    s.set_ylim([y_min - (y_max*PLOT_MARGIN), y_max + (y_max*PLOT_MARGIN)])
-                    s.set_xlim([x_min - (x_max*PLOT_MARGIN), x_max + (x_max*PLOT_MARGIN)])
-                    labels.append(d)
-                    figure.legend(labels=labels)
+                        s = sns.regplot(ax=axes[index_1, s_in], y=p_df.iloc[:, 0].name, x=d,
+                                        data=p_df, ci=None, truncate=False, robust=True, line_kws={'linewidth': 0.6}, scatter_kws={'alpha': 0.5})
+
+                        # y_delta = np.abs(np.abs(y_max) - np.abs(y_min)) * PLOT_MARGIN
+                        # x_delta = np.abs(np.abs(x_max) - np.abs(x_min)) * PLOT_MARGIN
+
+                        y_delta = 0.2
+                        x_delta = 1.4
+
+                        s.set_ylim((y_min - y_delta, y_max + y_delta))
+                        s.set_xlim((x_min - x_delta, x_max + x_delta))
+
             name = p_df.iloc[:, 0].name
-            name = (name[:44] + '..') if len(name) > 44 else name
+            name = (name[:40] + '..') if len(name) > 44 else name
             try:
                 int(name.split(",")[0])
                 name = "".join(name.split(",")[1:])
             except:
                 name = name
             axes[index_1, s_in].set_title(
-                name, fontdict={'fontsize': 25})
+                name, fontdict={'fontsize': 21})
             # axes[index_1, s_in].set_yticklabels([[0, 0.2, 0.4, 0.6, 0.8, 1.0]])
             # axes[index_1, s_in].set_xticklabels([0, 0.2, 0.4, 0.6, 0.8, 1.0])
             s.set(xlabel=" ")
@@ -149,12 +155,13 @@ def scatter_reg(filename, csv_filenames, plottable):
             # s.set_ylabel("€",horizontalalignment='right', y=1.05, labelpad=-16, fontsize=16)
             # s.set_xlabel("€",horizontalalignment='right', x=1.05, labelpad=-8, fontsize=16)
             s_in += 1
-            plt.show()
+            # plt.show()
 
     # plt.show()
     png_fname = filename.split(".")[0] + ".png"
     plt.tight_layout()
-    plt.savefig(f"plots/{png_fname}", format="png", bbox_inches='tight')
+
+    plt.savefig(f"plots/{png_fname}", format="png", bbox_inches='tight', dpi=300)
 
 
 def scatter(filename, csv_filenames, name_list):
@@ -247,13 +254,19 @@ def plot_names(dependent, csv_files, filename):
         truncate = True
         if len(set(dataframe[name])) > 1:
             truncate = False
-        s = sns.regplot(y=dependent, x=name, data=dataframe, ci=None, truncate=truncate, line_kws={'linewidth': 1.0})
+        plt.figure(figsize = (10, 8))
+        plt.xlim((-1.1, 16.5))
+        plt.ylim((4.6, 8.3))
+        s = sns.regplot(y=dependent, x=name, data=dataframe, ci=None, robust=True, truncate=truncate, line_kws={'linewidth': 1.0})
+
         xl = name.replace("%", "\%")
-        s.set_xlabel(f"{xl} (€)")
-        s.set_ylabel(f"{dependent} (€)")
+        s.set_xlabel(f"{xl} (€)", fontsize=26)
+        s.set_ylabel(f"{dependent} (€)", fontsize=26)
         fig = s.get_figure()
+
         filename = f"{dependent}_{name}"
         filename = "".join(x for x in filename if x.isalnum()) + ".png"
+        plt.tight_layout()
         try:
             fig.savefig(f"plot_t/{filename}", format="png", bbox_inches='tight', dpi=300)
         except:
@@ -280,16 +293,21 @@ plott = mean("correlation_pizza_to_beef.json", "pizza_to_beef_mean", get_top=Tru
 plott = mean("correlation_pizza_to_wheat_pig meat.json", "pizza_to_wheat_pig meat_mean", get_top=True)
 # scatter_reg("correlation_pizza_to_wheat_pig meat.json", ["datasets/pizza.csv", "datasets/pig meat.csv", "datasets/wheat.csv"], plott)
 
-# plott = mean("correlation_pizza_to_wheat_chicken.json", "pizza_to_wheat_chicken_mean", get_top=True)
-# scatter_reg("correlation_pizza_to_wheat_chicken.json", ["datasets/pizza.csv", "datasets/chicken.csv", "datasets/wheat.csv"], plott)
+plott = mean("correlation_pizza_to_wheat_pig meat.json", "pizza_to_wheat_pig meat_mean", get_top=True)
+scatter_reg("correlation_pizza_to_wheat_pig meat.json", ["datasets/pizza.csv", "datasets/pig meat.csv", "datasets/wheat.csv"], plott)
+
+plott = mean("correlation_pizza_to_wheat_chicken.json", "pizza_to_wheat_chicken_mean", get_top=True)
+scatter_reg("correlation_pizza_to_wheat_chicken.json", ["datasets/pizza.csv", "datasets/chicken.csv", "datasets/wheat.csv"], plott)
 
 plott = mean("correlation_cake_to_wheat_pear_apple.json", "cake_to_wheat_pear_apple_mean", get_top=True)
-# scatter_reg("correlation_cake_to_wheat_pear_apple.json", ["datasets/cake.csv", "datasets/wheat.csv", "datasets/pear.csv",  "datasets/apple.csv"], plott)
+scatter_reg("correlation_cake_to_wheat_pear_apple.json", ["datasets/cake.csv", "datasets/wheat.csv", "datasets/pear.csv",  "datasets/apple.csv"], plott)
 
 plott = mean("correlation_cake_to_wheat_milk.json", "cake_to_wheat_milk_mean", get_top=True)
-plott = mean("correlation_cookies_to_wheat_sugar_milk.json", "cookie_to_wheat_sugar_milk_mean", get_top=True)
-plott = mean("correlation_pizza_to_wheat_tomatoes_chicken.json", "pizza_to_wheat_tomatoes_chicken_mean", get_top=True)
-# scatter_reg("correlation_cake_to_wheat_milk.json", ["datasets/cake.csv", "datasets/wheat.csv", "datasets/milk.csv"], plott)
+scatter_reg("correlation_cake_to_wheat_milk.json", ["datasets/cake.csv", "datasets/wheat.csv", "datasets/milk.csv"], plott)
+print("Done saving scatters.")
 
 # scatter("correlation_rimi milk_to_other shop milk.json",
 #         ["datasets/rimi milk.csv", "datasets/other shop milk.csv"], topmost)
+
+
+# plt.style.use(['science', 'grayscale', 'grid', 'no-latex'])
